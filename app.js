d4d71677d1cfea31a2a8bf1135fc936b214f6c1b
Ian Nastajus <ian.nastajus@gmail.com>
2018-01-30 16:18:21 -0500

    rudimentary ejs <%= title %> syntax usage

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
     //res.send('hello');
-    res.render('index');
+    var title = 'Customers';
+    res.render('index', {
+        title: title
+    });
 });
 
 app.listen(3000, function(){
